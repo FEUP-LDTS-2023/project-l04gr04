dd22c75fe50380d736425a4128d20dc59d0931e3
Rei <up202207579@.up.pt>
2023-11-22 09:51:40 +0000

    Mapa criado

---
+++ src/main/java/org/example/Mapa.java
@@ -0,0 +1,85 @@
+package org.example;
+
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+
+import java.io.*;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Mapa {
+    private int width;
+    private int height;
+    private final String backgroundColor = "#000000";
+    private final String wallsColor = "#385A81";
+    private final String coinsColor = "#959043";
+    private long startTime;
+    private char[][] map;
+
+    public Mapa(int w , int h) throws IOException {
+        width = w;
+        height = h;
+        map = loadMapFromFile("map.txt");
+        startTime = System.currentTimeMillis();
+    }
+    public void draw(TextGraphics graphics) throws IOException {
+        graphics.setBackgroundColor(TextColor.Factory.fromString(backgroundColor));
+        graphics.setForegroundColor(TextColor.Factory.fromString("#2A0069"));
+        for (int row = 0; row < height; row++) {
+            for (int col = 0; col < width; col++) {
+                graphics.setBackgroundColor(TextColor.Factory.fromString(backgroundColor));
+                if (map[row][col] == '.') {
+                    graphics.fillRectangle(new TerminalPosition(col, row), new TerminalSize(1, 1), ' ');
+                } else if (map[row][col] == 'P') {
+                    graphics.setBackgroundColor(TextColor.Factory.fromString(wallsColor));
+                    graphics.fillRectangle(new TerminalPosition(col, row), new TerminalSize(1, 1), ' ');
+                } else if (map[row][col] == '0') {
+                    graphics.setBackgroundColor(TextColor.Factory.fromString(coinsColor));
+                    graphics.fillRectangle(new TerminalPosition(col, row), new TerminalSize(1, 1), ' ');
+                } else {
+                    graphics.setBackgroundColor(TextColor.Factory.fromString("#CC0066"));
+                    graphics.fillRectangle(new TerminalPosition(col, row), new TerminalSize(1, 1), ' ');
+                }
+            }
+        }
+    }
+
+
+    public char[][] loadMapFromFile(String filename) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(filename));
+        String line;
+        int numRows = height;
+        int numCols = width;
+        char[][] map = new char[numRows][numCols];
+        int row = 0;
+        boolean readingFile = true;
+        while (readingFile) {
+            line = reader.readLine();
+            char[] chars = line.toCharArray();
+            for (int col = 0; col < chars.length; col++) {
+                if (chars[col] == '!') {
+                    readingFile = false;
+                    break;
+                } else if (chars[col] == '#') {
+                    break;
+                } else {
+                    map[row][col] = chars[col];
+                }
+            }
+            row++;
+        }
+        reader.close();
+        return map;
+    }
+}
+
